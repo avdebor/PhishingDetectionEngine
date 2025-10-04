ec9d04c99e240073075818e37aa8e1b0250f7261
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2025-10-04 13:19:07 +0200

    Added a second project and added the project reference

---
+++ PhishingDetectionEngine.Core/Models/EmailAttachment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhishingDetectionEngine.Core.Models
+{
+    public class EmailAttachment
+    {
+        public string FileName { get; set; }       
+        public string ContentType { get; set; }    
+        public long Size { get; set; }             
+        public byte[] Content { get; set; }       
+    }
+}
+}
---
+++ PhishingDetectionEngine.Core/Models/ParsedEmail.cs
@@ -0,0 +1,14 @@
+﻿namespace PhishingDetectionEngine.Core.Models
+{
+    public class ParsedEmail
+    {
+        public string Subject { get; set; }
+        public string From { get; set; }          
+        public string To { get; set; }            
+        public string TextBody { get; set; }     
+        public string HtmlBody { get; set; }       
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+        public List<EmailAttachment> Attachments { get; set; } = new List<EmailAttachment>();
+    }
+
+}
