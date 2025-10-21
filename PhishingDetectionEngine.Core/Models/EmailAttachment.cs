bab9455b83ba1f8075f331fd00e954d57b8c9382
Yankov,Christiyan C.A. <c.yankov@student.fontys.nl>
2025-10-21 08:51:13 +0000

    fixed a typo

--- PhishingDetectionEngine.Core/Models/EmailAttachment.cs
+++ PhishingDetectionEngine.Core/Models/EmailAttachment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +13,4 @@ namespace PhishingDetectionEngine.Core.Models
         public long Size { get; set; }             
         public byte[] Content { get; set; }       
     }
-}
 }
